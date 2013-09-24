346ec8c0f009285ea1d156d5a34ce2da30b90307
Nick Lofaro <patchoulinick@gmail.com>
2013-09-24 12:27:40 -0400

    Hello this is my commit for my Rube Goldberg machine

---
+++ Assets/Camera1Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera1Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time>0f)
+		{
+			transform.position += new Vector3(2f, 0f, 0f)*Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter() {
+		Destroy(gameObject);
+		Destroy (GameObject.Find ("rod"));
+		light.enabled = true;
+	}
+}
